7c66000596b55363c10a6004261d072e00a43f4c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-01 22:47:47 +0900

    Split derive

---
+++ crates/derive-default-from-serde/src/lib.rs
@@ -0,0 +1,24 @@
+extern crate proc_macro;
+
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(SerdeDefault)]
+pub fn derive_default_from_serde(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics ::std::default::Default for #name #ty_generics #where_clause {
+            fn default() -> Self {
+                let json = r#"{}"#;
+                let mut deserializer = ::serde_json::Deserializer::from_str(json);
+                let t = <Self as ::serde::Deserialize>::deserialize(&mut deserializer).unwrap();
+                t
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
