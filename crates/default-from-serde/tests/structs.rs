2b217235c16315200255d8edba4aa5fe313b7e67
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-02 00:12:21 +0900

    test

--- crates/default-from-serde/tests/structs.rs
+++ crates/default-from-serde/tests/structs.rs
@@ -2,4 +2,22 @@ use derive_default_from_serde::SerdeDefault;
 use serde_derive::Deserialize;
 
 #[derive(SerdeDefault, Deserialize)]
-struct Struct1 {}
+struct Struct1 {
+    #[serde(default)]
+    field: String,
+
+    #[serde(default = "true_by_default")]
+    custom_default: bool,
+}
+
+fn true_by_default() -> bool {
+    true
+}
+
+#[test]
+fn test() {
+    let s = Struct1::default();
+
+    assert_eq!(s.field, String::default());
+    assert!(s.custom_default);
+}
