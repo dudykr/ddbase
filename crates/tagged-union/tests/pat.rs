88147d0794565ecdfe6db9e4e3702e04ecee5c4f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-14 13:13:55 +0900

    test

--- crates/tagged-union/tests/pat.rs
+++ crates/tagged-union/tests/pat.rs
@@ -1,4 +1,23 @@
 use tagged_union::TaggedUnion;
 
 #[derive(TaggedUnion)]
-enum Pat {}
+enum Pat {
+    Ident(Ident),
+    ArrayPat(ArrayPat),
+    ObjectPat(ObjectPat),
+    AssignPat(AssignPat),
+}
+
+#[derive(TaggedUnion)]
+enum AssignTargetPat {
+    ArrayPat(ArrayPat),
+    ObjectPat(ObjectPat),
+}
+
+struct Ident {}
+
+struct AssignPat {}
+
+struct ArrayPat {}
+
+struct ObjectPat {}
