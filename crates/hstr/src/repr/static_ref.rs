d36d20a5b659d98ee6eb22c49e2faa792a88026e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-11 13:24:44 +0900

    static str

--- crates/hstr/src/repr/static_ref.rs
+++ crates/hstr/src/repr/static_ref.rs
@@ -1,5 +1,6 @@
 use super::{nonmax::NonMaxUsize, Repr, KIND_STATIC};
 
+#[repr(C)]
 pub(super) struct StaticStr {
     ptr: *const u8,
     /// We use the last two bits to store the kind of the string.
@@ -8,11 +9,15 @@ pub(super) struct StaticStr {
 
 static_assertions::assert_eq_size!(Repr, StaticStr);
 
+const MAX_LEN: usize = (usize::MAX >> 2) - 1;
+
 impl StaticStr {
-    pub fn new(text: &'static str) -> Self {
+    // Safety: `text.len()` must be less than `usize::MAX >> 2 - 1`.
+    pub unsafe fn new(text: &'static str) -> Self {
         // Shift length to the right by 2 bits and store the kind in the last two
         // bits.
 
+        debug_assert!(text.len() < MAX_LEN);
         let len = NonMaxUsize::new(text.len() << 2 | (KIND_STATIC as usize));
 
         Self {
