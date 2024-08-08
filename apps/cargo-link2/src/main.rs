89b433aa1233b25291468064ac92833f52cd75b3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-08 15:31:24 +0900

    doc

--- apps/cargo-link2/src/main.rs
+++ apps/cargo-link2/src/main.rs
@@ -1,7 +1,15 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Debug, Parser)]
-struct CliArgs {}
+struct CliArgs {
+    /// The target directory to link to the current project.
+    ///
+    /// If the target directory is a cargo workspace, all packages in the
+    /// workspace will be linked.
+    target_dir: PathBuf,
+}
 
 fn main() {
     let args = CliArgs::parse();
