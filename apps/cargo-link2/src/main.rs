4620866155a342912edb8ff5dfe88c261811dad6
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-08 15:44:07 +0900

    WIP

--- apps/cargo-link2/src/main.rs
+++ apps/cargo-link2/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    env::current_dir,
     path::{Path, PathBuf},
     ptr::metadata,
 };
@@ -24,8 +25,19 @@ struct CliArgs {
 fn main() -> Result<()> {
     let args = CliArgs::parse();
 
+    let working_dir = match args.dir {
+        Some(v) => v,
+        None => current_dir().context("failed to get current directory")?,
+    };
+
     let link_candidates =
         list_of_crates(&args.target_dir).context("failed to get candidates for linking")?;
+
+    let crate_names = add_patch_section(&working_dir, &link_candidates)
+        .context("failed to add patch section to Cargo.toml")?;
+
+    run_cargo_update(&working_dir, &crate_names)
+        .context("failed to run cargo update in the working directory")?;
 
     Ok(())
 }
@@ -44,4 +56,28 @@ fn list_of_crates(target_dir: &Path) -> Result<Vec<String>> {
         .filter(|p| ws_members.contains(&p.id))
         .map(|p| p.name)
         .collect())
+}
+
+fn add_patch_section(working_dir: &Path, link_candidates: &[String]) -> Result<Vec<String>> {}
+
+fn run_cargo_update(dir: &PathBuf, crate_names: &[String]) -> Result<()> {
+    let mut cmd = std::process::Command::new(cargo_bin());
+    cmd.current_dir(dir);
+    cmd.arg("update");
+    for name in crate_names {
+        cmd.arg("--package");
+        cmd.arg(name);
+    }
+
+    let status = cmd.status().context("failed to run cargo update")?;
+
+    if !status.success() {
+        anyhow::bail!("cargo update failed with status: {}", status);
+    }
+
+    Ok(())
+}
+
+fn cargo_bin() -> String {
+    std::env::var("CARGO").unwrap_or_else(|_| "cargo".to_string())
 }
