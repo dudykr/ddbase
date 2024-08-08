746799aee9936990747c827e08b6b6753d2e9ed9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-08 15:33:51 +0900

    doc

--- apps/cargo-link2/src/main.rs
+++ apps/cargo-link2/src/main.rs
@@ -4,6 +4,12 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 struct CliArgs {
+    /// Changes the link location to <dir>.
+    ///
+    /// Defaults to the current directory.
+    #[clap(short = 'C', long)]
+    dir: Option<PathBuf>,
+
     /// The target directory to link to the current project.
     ///
     /// If the target directory is a cargo workspace, all packages in the
