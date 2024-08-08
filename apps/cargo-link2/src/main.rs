892dc283122503a3cbffc3dda7732ffca372bad4
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-08-08 17:14:34 +0900

    feat(cargo-link): Rename binary and make it usable as `cargo link` (#42)

--- apps/cargo-link2/src/main.rs
+++ apps/cargo-link2/src/main.rs
@@ -8,8 +8,20 @@ use anyhow::{Context, Result};
 use cargo_metadata::{Metadata, MetadataCommand};
 use clap::Parser;
 
+cargo_subcommand_metadata::description!(
+    "Link crates from a cargo workspace to the current project"
+);
+
+#[derive(Parser)]
+#[command(bin_name = "cargo", version, author, disable_help_subcommand = true)]
+enum Subcommand {
+    /// Show the result of macro expansion.
+    #[command(name = "link", version, author, disable_version_flag = true)]
+    Link(Link),
+}
+
 #[derive(Debug, Parser)]
-struct CliArgs {
+struct Link {
     /// Changes the link location to <dir>.
     ///
     /// Defaults to the current directory.
@@ -24,7 +36,7 @@ struct CliArgs {
 }
 
 fn main() -> Result<()> {
-    let args = CliArgs::parse();
+    let Subcommand::Link(args) = Subcommand::parse();
 
     let working_dir = match args.dir {
         Some(v) => v,
