e4377d3ddaa1f048eaf45e49258eb26df2264df9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-07 06:53:07 +0900

    almost

--- crates/shrink-to-fit-macro/src/lib.rs
+++ crates/shrink-to-fit-macro/src/lib.rs
@@ -8,9 +8,39 @@ pub fn derive_shrink_to_fit(input: proc_macro::TokenStream) -> proc_macro::Token
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
     let body_impl = match &input.data {
-        syn::Data::Struct(s) => {}
+        syn::Data::Struct(s) => {
+            let (field_bindings, body_code) = expand_fields(&s.fields);
 
-        syn::Data::Enum(e) => {}
+            quote!(
+                match self {
+                    Self { #field_bindings } => {
+                        #body_code
+                    }
+                }
+            )
+        }
+
+        syn::Data::Enum(e) => {
+            let mut arms = proc_macro2::TokenStream::new();
+
+            for v in e.variants.iter() {
+                let variant_name = &v.ident;
+
+                let (field_bindings, body_code) = expand_fields(&v.fields);
+
+                arms.extend(quote!(
+                    Self::#variant_name { #field_bindings } => {
+                        #body_code
+                    },
+                ));
+            }
+
+            quote!(
+                match self {
+                    #arms
+                }
+            )
+        }
 
         syn::Data::Union(u) => {
             panic!("union is not supported: {:?}", u);
@@ -24,4 +54,11 @@ pub fn derive_shrink_to_fit(input: proc_macro::TokenStream) -> proc_macro::Token
             }
         }
     }
+    .into()
+}
+
+/// Returns `(field_bindings, body_code)`
+fn expand_fields(fields: &syn::Fields) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+    let mut body_impl = proc_macro2::TokenStream::new();
+    let mut body_drop = proc_macro2::TokenStream::new();
 }
