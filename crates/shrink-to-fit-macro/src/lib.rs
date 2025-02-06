c51895f54258b78b1638af1026418710c23d10d1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-07 06:47:58 +0900

    body_impl

--- crates/shrink-to-fit-macro/src/lib.rs
+++ crates/shrink-to-fit-macro/src/lib.rs
@@ -1,6 +1,25 @@
 #[proc_macro_derive(ShrinkToFit)]
 pub fn derive_shrink_to_fit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let _input: syn::DeriveInput = syn::parse_macro_input!(input as syn::DeriveInput);
+    let input: syn::DeriveInput = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    panic!("todo!")
+    let name = &input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let body_impl = match &input.data {
+        syn::Data::Struct(s) => {}
+
+        syn::Data::Enum(e) => {}
+
+        syn::Data::Union(u) => {
+            panic!("union is not supported: {:?}", u);
+        }
+    };
+
+    quote! {
+        impl<#impl_generics> ShrinkToFit for #name<#ty_generics> #where_clause {
+            fn shrink_to_fit(&mut self) {
+                #body_impl
+            }
+        }
+    }
 }
