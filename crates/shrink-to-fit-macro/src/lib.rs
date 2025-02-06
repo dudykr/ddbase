62e97191d59a4c1b241f4f91ee6a483abc9b044a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-07 06:48:10 +0900

    quote

--- crates/shrink-to-fit-macro/src/lib.rs
+++ crates/shrink-to-fit-macro/src/lib.rs
@@ -1,3 +1,5 @@
+use quote::quote;
+
 #[proc_macro_derive(ShrinkToFit)]
 pub fn derive_shrink_to_fit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input: syn::DeriveInput = syn::parse_macro_input!(input as syn::DeriveInput);
