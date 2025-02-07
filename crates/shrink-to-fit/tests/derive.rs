d4f72c93608ffd1531cea9f61333c0f352fc84c7
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-07 10:00:47 +0900

    test

--- crates/shrink-to-fit/tests/derive.rs
+++ crates/shrink-to-fit/tests/derive.rs
@@ -21,3 +21,26 @@ fn test_shrink_to_fit() {
     assert_eq!(s.a.capacity(), 1);
     assert_eq!(s.b.capacity(), 1);
 }
+
+#[derive(Debug, ShrinkToFit)]
+struct AutoDerefSpecialization {
+    a: String,
+    b: NotImplementShrinkToFit,
+}
+
+#[derive(Debug)]
+struct NotImplementShrinkToFit;
+
+#[test]
+fn test_auto_deref_specialization() {
+    let mut s = AutoDerefSpecialization {
+        a: String::with_capacity(100),
+        b: NotImplementShrinkToFit,
+    };
+
+    s.a.push('a');
+
+    s.shrink_to_fit();
+
+    assert_eq!(s.a.capacity(), 1);
+}
