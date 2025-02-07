8da9ec53d207c667f123c6cace9a54b7dff78f78
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-07 13:56:29 +0900

    macro

--- crates/shrink-to-fit-macro/src/lib.rs
+++ crates/shrink-to-fit-macro/src/lib.rs
@@ -1,17 +1,17 @@
-use quote::quote;
-use syn::{spanned::Spanned, Attribute, Ident};
+use quote::{quote, ToTokens};
+use syn::{spanned::Spanned, Attribute, Expr, Ident, Lit, Meta};
 
 #[proc_macro_derive(ShrinkToFit, attributes(shrink_to_fit))]
 pub fn derive_shrink_to_fit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input: syn::DeriveInput = syn::parse_macro_input!(input as syn::DeriveInput);
-    let data_attr: DataAttr = DataAttr::parse(&input.attrs);
+    let type_attr: TypeAttr = TypeAttr::parse(&input.attrs);
 
     let name = &input.ident;
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
     let body_impl = match &input.data {
         syn::Data::Struct(s) => {
-            let (field_bindings, body_code) = expand_fields(&s.fields);
+            let (field_bindings, body_code) = expand_fields(&type_attr, &s.fields);
 
             quote!(
                 match self {
@@ -28,7 +28,7 @@ pub fn derive_shrink_to_fit(input: proc_macro::TokenStream) -> proc_macro::Token
             for v in e.variants.iter() {
                 let variant_name = &v.ident;
 
-                let (field_bindings, body_code) = expand_fields(&v.fields);
+                let (field_bindings, body_code) = expand_fields(&type_attr, &v.fields);
 
                 arms.extend(quote!(
                     Self::#variant_name { #field_bindings } => {
@@ -60,16 +60,29 @@ pub fn derive_shrink_to_fit(input: proc_macro::TokenStream) -> proc_macro::Token
 }
 
 #[derive(Default)]
-struct DataAttr {
+struct TypeAttr {
     crate_name: Option<syn::Path>,
 }
-impl DataAttr {
-    fn parse(attrs: &[Attribute]) -> DataAttr {
-        let mut data_attr = DataAttr::default();
+impl TypeAttr {
+    fn parse(attrs: &[Attribute]) -> TypeAttr {
+        let mut data_attr = TypeAttr::default();
 
         for attr in attrs {
-            if attr.path().is_ident("crate") {
-                data_attr.crate_name = Some(attr.path().clone());
+            if attr.path().is_ident("shrink_to_fit") {
+                if let Meta::List(meta) = &attr.meta {
+                    let tokens = meta.tokens.clone();
+                    let kv = syn::parse2::<syn::MetaNameValue>(tokens).unwrap();
+
+                    if kv.path.is_ident("crate") {
+                        if let Expr::Lit(syn::ExprLit {
+                            lit: Lit::Str(s), ..
+                        }) = &kv.value
+                        {
+                            let path = syn::parse_str::<syn::Path>(&s.value()).unwrap();
+                            data_attr.crate_name = Some(path);
+                        }
+                    }
+                }
             }
         }
 
@@ -78,7 +91,16 @@ impl DataAttr {
 }
 
 /// Returns `(field_bindings, body_code)`
-fn expand_fields(fields: &syn::Fields) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+fn expand_fields(
+    type_attr: &TypeAttr,
+    fields: &syn::Fields,
+) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+    let crate_name = type_attr
+        .crate_name
+        .as_ref()
+        .map(|q| q.to_token_stream())
+        .unwrap_or_else(|| quote!(shrink_to_fit));
+
     let mut field_bindings = proc_macro2::TokenStream::new();
     let mut body_impl = proc_macro2::TokenStream::new();
 
@@ -92,7 +114,7 @@ fn expand_fields(fields: &syn::Fields) -> (proc_macro2::TokenStream, proc_macro2
                 ));
 
                 body_impl.extend(quote!(
-                    shrink_to_fit::helpers::ShrinkToFitDerefSpecialization::new(#field_name).shrink_to_fit();
+                    #crate_name::helpers::ShrinkToFitDerefSpecialization::new(#field_name).shrink_to_fit();
                 ));
             }
         }
@@ -102,7 +124,7 @@ fn expand_fields(fields: &syn::Fields) -> (proc_macro2::TokenStream, proc_macro2
                 let field_name = Ident::new(&format!("_{}", i), field.span());
 
                 body_impl.extend(quote!(
-                    shrink_to_fit::helpers::ShrinkToFitDerefSpecialization::new(#field_name).shrink_to_fit()
+                    #crate_name::helpers::ShrinkToFitDerefSpecialization::new(#field_name).shrink_to_fit()
                 ));
 
                 let index = syn::Index::from(i);
