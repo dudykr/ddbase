9a70ae132e3130bea5470c9049a7cc3861a98290
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-26 16:12:06 +0900

    Document

--- crates/par-core/src/lib.rs
+++ crates/par-core/src/lib.rs
@@ -1,3 +1,56 @@
+//! # par-core
+//!
+//! A wrapper for various parallelization library for Rust.
+//! This crate currently supports
+//!
+//! - [`chili`](https://github.com/dragostis/chili)
+//! - [`rayon`](https://github.com/rayon-rs/rayon)
+//! - Disable parallelization.
+//!
+//! # Usage
+//!
+//! If you are developing a library, you should not force the parallelization
+//! library, and let the users choose the parallelization library.
+//!
+//! ## Final application
+//!
+//! If you are developing a final application, you can use cargo feature to
+//! select the parallelization library.
+//!
+//! ### `chili`
+//!
+//! ```toml
+//! [dependencies]
+//! par-core = { version = "1.0.1", features = ["chili"] }
+//! ```
+//!
+//! ### `rayon`
+//!
+//! ```toml
+//! [dependencies]
+//! par-core = { version = "1.0.1", features = ["rayon"] }
+//! ```
+//!
+//! ### Disable parallelization
+//!
+//! ```toml
+//! [dependencies]
+//! par-core = { version = "1.0.1", default-features = false }
+//! ```
+//!
+//! ## Library developers
+//!
+//! If you are developing a library, you can simply depend on `par-core` without
+//! any features. **Note**: To prevent a small mistake of end-user making the
+//! appplication slower, `par-core` emits a error message using a default
+//! feature. So if you are a library developer, you should specify
+//! `default-features = false`.
+//!
+//! ```toml
+//! [dependencies]
+//! par-core = { version = "1.0.1", default-features = false }
+//! ```
+
 #![cfg_attr(not(feature = "chili"), allow(unused_variables))]
 
 use std::{cell::RefCell, mem::transmute};
