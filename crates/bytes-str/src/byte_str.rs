976abf742ad2e956de31326f4474fc94dfb43e49
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-06 08:20:18 -0700

    more traits

--- crates/bytes-str/src/byte_str.rs
+++ crates/bytes-str/src/byte_str.rs
@@ -1,7 +1,45 @@
+use std::{borrow::Borrow, ops::Deref};
+
 use bytes::Bytes;
 
 /// [str], but backed by [Bytes].
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct BytesStr {
     bytes: Bytes,
+}
+
+impl Deref for BytesStr {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+
+impl AsRef<str> for BytesStr {
+    fn as_ref(&self) -> &str {
+        unsafe { std::str::from_utf8_unchecked(&self.bytes) }
+    }
+}
+
+impl Borrow<str> for BytesStr {
+    fn borrow(&self) -> &str {
+        self.as_ref()
+    }
+}
+
+impl From<String> for BytesStr {
+    fn from(s: String) -> Self {
+        Self {
+            bytes: Bytes::from(s),
+        }
+    }
+}
+
+impl From<&'static str> for BytesStr {
+    fn from(s: &'static str) -> Self {
+        Self {
+            bytes: Bytes::from_static(s.as_bytes()),
+        }
+    }
 }
--- crates/bytes-str/src/byte_string.rs
+++ crates/bytes-str/src/byte_string.rs
@@ -1,7 +1,54 @@
-use bytes::BytesMut;
+use std::{
+    borrow::Borrow,
+    ops::{Deref, DerefMut},
+};
+
+use bytes::{Bytes, BytesMut};
 
 /// [String] but backed by a [BytesMut]
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct ByteString {
     bytes: BytesMut,
+}
+
+impl Deref for ByteString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { std::str::from_utf8_unchecked(&self.bytes) }
+    }
+}
+
+impl DerefMut for ByteString {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { std::str::from_utf8_unchecked_mut(&mut self.bytes) }
+    }
+}
+
+impl AsRef<str> for ByteString {
+    fn as_ref(&self) -> &str {
+        self.deref()
+    }
+}
+
+impl Borrow<str> for ByteString {
+    fn borrow(&self) -> &str {
+        self.as_ref()
+    }
+}
+
+impl From<String> for ByteString {
+    fn from(s: String) -> Self {
+        Self {
+            bytes: Bytes::from(s.into_bytes()).into(),
+        }
+    }
+}
+
+impl From<&str> for ByteString {
+    fn from(s: &str) -> Self {
+        Self {
+            bytes: BytesMut::from(s),
+        }
+    }
 }
