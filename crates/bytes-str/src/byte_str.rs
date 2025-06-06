d5009305f7d71670c1347646ea023cfee128fca9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-06 09:22:05 -0700

    more api

--- crates/bytes-str/src/byte_str.rs
+++ crates/bytes-str/src/byte_str.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Borrow, ops::Deref};
+use std::{borrow::Borrow, ffi::OsStr, ops::Deref, path::Path, str::Utf8Error};
 
 use bytes::Bytes;
 
@@ -10,7 +10,56 @@ pub struct BytesStr {
     pub(crate) bytes: Bytes,
 }
 
-impl BytesStr {}
+impl BytesStr {
+    pub fn new() -> Self {
+        Self {
+            bytes: Bytes::new(),
+        }
+    }
+
+    pub fn from_static(bytes: &'static str) -> Self {
+        Self {
+            bytes: Bytes::from_static(bytes.as_bytes()),
+        }
+    }
+
+    pub fn from_utf8(bytes: Bytes) -> Result<Self, Utf8Error> {
+        std::str::from_utf8(&bytes)?;
+
+        Ok(Self { bytes })
+    }
+
+    pub fn from_utf8_slice(bytes: &[u8]) -> Result<Self, Utf8Error> {
+        std::str::from_utf8(bytes)?;
+
+        Ok(Self {
+            bytes: Bytes::copy_from_slice(bytes),
+        })
+    }
+
+    pub fn from_static_utf8_slice(bytes: &'static [u8]) -> Result<Self, Utf8Error> {
+        std::str::from_utf8(bytes)?;
+
+        Ok(Self {
+            bytes: Bytes::from_static(bytes),
+        })
+    }
+
+    /// Returns a string slice containing the entire BytesStr.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use bytes_str::BytesStr;
+    ///
+    /// let s = BytesStr::from_static("hello");
+    ///
+    /// assert_eq!(s.as_str(), "hello");
+    /// ```
+    pub fn as_str(&self) -> &str {
+        unsafe { std::str::from_utf8_unchecked(&self.bytes) }
+    }
+}
 
 impl Deref for BytesStr {
     type Target = str;
@@ -22,13 +71,13 @@ impl Deref for BytesStr {
 
 impl AsRef<str> for BytesStr {
     fn as_ref(&self) -> &str {
-        unsafe { std::str::from_utf8_unchecked(&self.bytes) }
+        self.as_str()
     }
 }
 
 impl Borrow<str> for BytesStr {
     fn borrow(&self) -> &str {
-        self.as_ref()
+        self.as_str()
     }
 }
 
@@ -61,5 +110,29 @@ impl From<BytesString> for BytesStr {
         Self {
             bytes: s.bytes.into(),
         }
+    }
+}
+
+impl AsRef<[u8]> for BytesStr {
+    fn as_ref(&self) -> &[u8] {
+        self.bytes.as_ref()
+    }
+}
+
+impl AsRef<Bytes> for BytesStr {
+    fn as_ref(&self) -> &Bytes {
+        &self.bytes
+    }
+}
+
+impl AsRef<OsStr> for BytesStr {
+    fn as_ref(&self) -> &OsStr {
+        OsStr::new(self.as_str())
+    }
+}
+
+impl AsRef<Path> for BytesStr {
+    fn as_ref(&self) -> &Path {
+        Path::new(self.as_str())
     }
 }
