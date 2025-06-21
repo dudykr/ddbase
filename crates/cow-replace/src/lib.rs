bbdffa6baae059674dacf6fd79777efa69a02237
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-22 07:03:28 +0900

    trait defs

--- crates/cow-replace/src/lib.rs
+++ crates/cow-replace/src/lib.rs
@@ -1,1 +1,17 @@
+use std::{borrow::Cow, mem::take};
 
+use ascii::AsciiChar;
+
+pub trait ReplaceString {
+    fn remove_all_ascii(&self, ch: AsciiChar) -> Cow<'_, str>;
+
+    fn remove_all_ascii_in_place(&mut self, ch: AsciiChar);
+
+    fn replace_all_ascii_in_place(&mut self, from: AsciiChar, to: AsciiChar);
+
+    fn replace_all_str(&mut self, from: &str, to: &str) -> Cow<'_, str>;
+}
+
+impl ReplaceString for String {}
+
+impl ReplaceString for Cow<'_, str> {}
