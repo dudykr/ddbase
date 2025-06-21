60293e52c00e030d82f932c34d4ae6fa55c84dd1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-22 07:33:59 +0900

    review

--- crates/cow-replace/src/lib.rs
+++ crates/cow-replace/src/lib.rs
@@ -214,22 +214,6 @@ impl ReplaceStringInPlace for String {
     }
 }
 
-impl ReplaceString for Cow<'_, str> {
-    fn remove_all_ascii(&self, ch: AsciiChar) -> Cow<'_, str> {
-        match remove_ascii_from_str(self, ch) {
-            Some(result) => Cow::Owned(result),
-            None => Cow::Borrowed(self),
-        }
-    }
-
-    fn replace_all_str(&self, from: &str, to: &str) -> Cow<'_, str> {
-        match replace_str_if_contains(self, from, to) {
-            Some(result) => Cow::Owned(result),
-            None => Cow::Borrowed(self),
-        }
-    }
-}
-
 impl ReplaceStringInPlace for Cow<'_, str> {
     fn remove_all_ascii_in_place(&mut self, ch: AsciiChar) {
         match self {
