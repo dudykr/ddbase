5e3ed8b5aa503ffdc52d0f80d0a8243196876018
Devon Govett <devongovett@gmail.com>
2026-08-21 11:58:37 -0700

    switch to cargo feature flag

--- crates/is-macro/src/lib.rs
+++ crates/is-macro/src/lib.rs
@@ -248,7 +248,31 @@ fn expand(input: DataEnum) -> Vec<ImplItem> {
                 }
 
                 let variant = &v.ident;
-
+                let expect_panic_arm: syn::Arm = if cfg!(feature = "small-panic") {
+                    parse_quote!(
+                        _ => {
+                            // Omit Debug formatting in release builds to reduce binary size.
+                            #[cfg(debug_assertions)]
+                            panic!(
+                                concat!("called ", stringify!(#name_of_expect), " on {:?}"),
+                                self
+                            );
+                            #[cfg(not(debug_assertions))]
+                            panic!(concat!(
+                                "called ",
+                                stringify!(#name_of_expect),
+                                " on another variant",
+                            ));
+                        }
+                    )
+                } else {
+                    parse_quote!(
+                        _ => panic!(
+                            concat!("called ", stringify!(#name_of_expect), " on {:?}"),
+                            self
+                        )
+                    )
+                };
                 let item_impl: ItemImpl = parse_quote!(
                     impl #ty {
                         #[doc = #docs_of_cast]
@@ -277,24 +301,7 @@ fn expand(input: DataEnum) -> Vec<ImplItem> {
                         {
                             match self {
                                 Self::#variant(#fields) => (#fields),
-                                _ => {
-                                  // Keep debug formatting out of release builds to reduce binary size.
-                                  #[cfg(debug_assertions)]
-                                  panic!(
-                                      concat!(
-                                          "called ",
-                                          stringify!(#name_of_expect),
-                                          " on {:?}",
-                                      ),
-                                      self
-                                  );
-                                  #[cfg(not(debug_assertions))]
-                                  panic!(concat!(
-                                      "called ",
-                                      stringify!(#name_of_expect),
-                                      " on another variant",
-                                  ));
-                                },
+                                #expect_panic_arm
                             }
                         }
 
